627e8ec1903aea34167e413bda3d783c736a91df
nguyenlam1962c5 <120208210+nguyenlam1962c5@users.noreply.github.com>
2025-01-11 06:26:43 +0700

    062611012025/ da sua xong di chuyen keyboard vs button

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -19,8 +19,14 @@ public class PlayerController : MonoBehaviour
     {
         // Xử lý input cho di chuyển ngang
         float inputAxis = 0f;
-        if (moveLeft) inputAxis = -1f;
-        if (moveRight) inputAxis = 1f;
+        if (moveLeft)
+        {
+            inputAxis = -1f;
+        }
+        else if (moveRight)
+        {
+            inputAxis = 1f;
+        }
 
         // Gửi giá trị input trục ngang tới PlayerMovement
         playerMovement.SetHorizontalInput(inputAxis);
@@ -34,8 +40,16 @@ public class PlayerController : MonoBehaviour
     }
 
     // Các phương thức cho nút di chuyển trái
-    public void OnLeftButtonDown() => moveLeft = true;
-    public void OnLeftButtonUp() => moveLeft = false;
+    public void OnLeftButtonDown()
+    {
+        moveLeft = true;
+        Debug.Log("Left button pressed");
+    }
+    public void OnLeftButtonUp()
+    {
+        moveLeft = false;
+        Debug.Log("Left button released");
+    }
 
     // Các phương thức cho nút di chuyển phải
     public void OnRightButtonDown() => moveRight = true;
@@ -43,10 +57,8 @@ public class PlayerController : MonoBehaviour
 
     // Các phương thức cho nút nhảy
     public void OnJumpButtonDown() => jump = true; // Bắt đầu nhảy
-
 }
-
-/*
+*/
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -63,36 +75,43 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Di chuyển qua các nút cảm ứng hoặc bàn phím
-        HandleMovementInput();
+        // Xử lý input từ keyboard
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveLeft = true;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveRight = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
 
-        // Nhảy qua các nút cảm ứng hoặc bàn phím
-        HandleJumpInput();
+        // Xử lý input axis
+        float inputAxis = 0f;
+        if (moveLeft) inputAxis = -1f;
+        if (moveRight) inputAxis = 1f;
 
-        // Truyền dữ liệu vào PlayerMovement
-        playerMovement.SetHorizontalInput(moveLeft ? -1f : moveRight ? 1f : 0f);
+        // Gửi giá trị input tới PlayerMovement
+        playerMovement.SetHorizontalInput(inputAxis);
 
-        // Thực hiện nhảy
+        // Xử lý nhảy
         if (jump)
         {
             playerMovement.Jump();
             jump = false;
         }
-    }
 
-    private void HandleMovementInput()
-    {
-        // Input từ bàn phím
-        moveLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
-        moveRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
-    }
-
-    private void HandleJumpInput()
-    {
-        // Nhảy với bàn phím
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Reset các giá trị khi thả phím
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            jump = true;
+            moveLeft = false;
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moveRight = false;
         }
     }
 
@@ -100,11 +119,10 @@ public class PlayerController : MonoBehaviour
     public void OnLeftButtonDown() => moveLeft = true;
     public void OnLeftButtonUp() => moveLeft = false;
 
-    // Các phương thức cho nút di chuyển phải
+    // Các phương thức cho nút di chuyển phải 
     public void OnRightButtonDown() => moveRight = true;
     public void OnRightButtonUp() => moveRight = false;
 
     // Phương thức cho nút nhảy
     public void OnJumpButtonDown() => jump = true;
 }
-*/
