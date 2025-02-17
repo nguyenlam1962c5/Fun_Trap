8c979a175a517fa815203256f8a8cc0094df82b6
nguyenlam1962c5 <120208210+nguyenlam1962c5@users.noreply.github.com>
2025-02-17 10:23:58 +0700

    102317022025/ update background music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioSource audioSource; // Kéo Audio Source vào đây
+
+    void Start()
+    {
+        audioSource.Play(); // Bắt đầu phát nhạc
+    }
+}
